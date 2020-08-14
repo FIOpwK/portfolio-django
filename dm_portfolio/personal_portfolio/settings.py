c2331c5f2e9a8fd64362194fa393fac153fc0be1
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-08-14 16:39:04 -0400

    removed secrets and debug

--- dm_portfolio/personal_portfolio/settings.py
+++ dm_portfolio/personal_portfolio/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kcoqy@ggq7_ib=vf)so=*9ota_^ml9t)!v!fj*kk3f$eonyj!2'
+SECRET_KEY = '8859327832945257964219112'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
